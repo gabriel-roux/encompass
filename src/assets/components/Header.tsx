2531e35881f0327393480988160e81bdf9c5dac6
Gabriel Fonseca <fonsecagabriel9781@gmail.com>
2022-11-28 07:08:26 -0300

    new containers

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,41 +1,124 @@
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import Logo from '../imgs/logo.svg'
 
+import Slider from 'react-slick'
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function Header() {
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1
+    };
 
     return (
-        <header>
-            <Container fluid="sm">
-                <Row>
-                    <Col sm={6}>
+        <>
+            <header style={{ height: '100vh', backgroundImage: `url(https://i.imgur.com/9JxM6Wo.png)`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                <div className='headerLine'>
+                    <h1>Conheça o método pdz que te faz faturar pelo menos R$1.000,00 por dia com plr apenas usando a <br /> internet sem precisar aparecer e conquiste sua liberdade financeira nos próximos 3 meses</h1>
+                </div>
+                <Container fluid="sm">
+                    <Row>
+                        <Col sm={12}>
+                            <h1 className="titulo2">
+                                VOU TE <span>REVELAR</span> TUDO QUE EU SEI E <span>TE ENSINAR PLR DO ZERO</span> PARA VOCÊ
+                                SAIR DO <span>ZERO ABSOLUTO</span> ATÉ O <span>PRIMEIRO MILHÃO</span>
+                            </h1>
+                            <div style={{ width: '100%', margin: '3rem auto' }}>
+                                <iframe style={{ margin: '3rem auto', display: 'block' }} width="780" height="460" src="https://www.youtube.com/embed/8D3giJhahmM" title="MONARK IMPRESSIONA CARIANI COM PERGUNTA" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            </div>
 
-                        <Image src={Logo}
-                            alt="Logo encompass"
-                            style={{ marginLeft: '95px', marginTop: '-25px' }}
-                            width={160}
-                            height={140}
-                            quality={100} />
+                            <a>SIM! QUERO FAZER PARTE DA PRIMEIRA TURMA AGORA!</a>
 
-                    </Col>
-                    <Col>
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
 
-                        <ul className="reduxParagraph3">
-                            <li> <label style={{ fontWeight: 500 }}>PLR do ZERO</label></li>
-                            <li> <label style={{ fontWeight: 300 }}>Como funciona</label></li>
-                            <li> <label style={{ fontWeight: 300 }}>Ensino</label></li>
-                            <li> <label style={{ fontWeight: 300 }}>Tire suas duvidas</label></li>
-                            <li> <label style={{ fontWeight: 300 }}>Depoimentos</label></li>
-                        </ul>
+            <div className='depoimentos'>
+                <h1 className="titulo2">
+                    Esses são as aprovações que <span>nós recebemos.</span>
+                    <br />
+                    <a>DA PRÓXIMA VEZ O SEU PODE ESTAR AQUI!</a>
 
-                    </Col>
-                </Row>
-            </Container>
-        </header>
 
+
+                    <div style={{ marginTop: '5rem', overflow: 'hidden' }}>
+                        <video width="540" height="100%" controls>
+                            <source src="https://i.imgur.com/33njvw2.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <Slider style={{ marginTop: '4rem', marginLeft: '-5rem' }} {...settings}>
+                            <div>
+                                <img
+                                    alt='depoimento 1'
+                                    src='https://i.imgur.com/32dJP98.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 2'
+                                    src='https://i.imgur.com/28WN8uu.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 3'
+                                    src='https://i.imgur.com/EYRv5wL.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 4'
+                                    src='https://i.imgur.com/9bAsOra.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 5'
+                                    src='https://i.imgur.com/z5NIYfh.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 6'
+                                    src='https://i.imgur.com/aDnsGpE.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    alt='depoimento 7'
+                                    src='https://i.imgur.com/o9SmQR5.png'
+                                    width={380}
+                                    height={550}
+                                />
+                            </div>
+                        </Slider>
+                    </div>
+                </h1>
+            </div>
+
+        </>
     )
 
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,42 @@ import { Header } from "../assets/components/Header";
 import { PLR } from "../assets/components/PLR";
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import { Rodape } from "../assets/components/Rodape";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [canShow, setCanShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+
+      setCanShow(true)
+
+    }, 30000)
+  }, [])
+
+
+
   return (
     <div style={{ width: '100%', height: 'auto', background: '#050507' }}>
 
       <Header />
-      <PLR />
-      <Apendizagem />
-      <Caracteristicas />
-      <Conteudo />
-      <Enzo />
-      <Depoimentos />
-      <FAQ />
 
-      <Rodape />
+      {
+        canShow ?
+          <>
+            <PLR />
+            <Apendizagem />
+            <Caracteristicas />
+            <Conteudo />
+            <Enzo />
+            <Depoimentos />
+            <FAQ />
+
+            <Rodape />
+          </>
+          : 
+          ''
+      }
 
       <a
         target="_blank"
@@ -32,8 +53,8 @@ export default function Home() {
           sx={{
             fontSize: '36px',
             color: '#fff'
-          }} 
-          />
+          }}
+        />
       </a>
     </div>
   )
